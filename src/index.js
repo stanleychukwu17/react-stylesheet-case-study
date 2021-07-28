cbf3d23fc8b0b4c48de76791a3f7be2b27f61482
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-28 07:49:47 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,5 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
