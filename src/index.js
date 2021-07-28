8f1c5b123e2599a181d3339e163a50d77848e4a7
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-28 07:49:59 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
